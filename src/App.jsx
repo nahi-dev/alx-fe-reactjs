ed067023c9fd32faebd603297bdfe4f364290d2a
ABILA <nahiltadesse01@gmail.com>
2025-07-07 14:55:29 +0300

    added UserProfile.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+import UserProfile from "./components/UserProfile";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </>
+  );
+}
+
+export default App;
