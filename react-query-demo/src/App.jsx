733f7a3ffceb1e6a27b2f57e41384f091ce93b19
ABILA <nahiltadesse01@gmail.com>
2025-08-24 14:55:16 +0300

    first commit

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import PostsComponent from "./components/PostsComponent.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
