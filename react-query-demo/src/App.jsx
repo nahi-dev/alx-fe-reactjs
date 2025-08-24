6df6ee26541ff2a8399a976b3f35cfdbe158e3e4
ABILA <nahiltadesse01@gmail.com>
2025-08-24 14:56:45 +0300

    second commit

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,11 +1,17 @@
-import PostsComponent from "./components/PostsComponent.jsx";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,14 +1,10 @@
-// src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App.jsx";
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </QueryClientProvider>
+  </StrictMode>
 );
