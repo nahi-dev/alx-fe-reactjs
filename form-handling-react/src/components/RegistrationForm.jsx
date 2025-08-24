391f9483b171661c0a63023016ab4fd5887eff5f
ABILA <nahiltadesse01@gmail.com>
2025-08-24 14:15:01 +0300

    first commit

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, email, password } = formData;
+
+    if (!username || !email || !password) {
+      setError("All fields are required.");
+      return;
+    }
+
+    setError("");
+    console.log("Form Submitted:", formData);
+    alert(`User Registered: ${username}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>User Registration (Controlled Components)</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <div>
+        <label>Username: </label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label>Email: </label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label>Password: </label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
